d6769ea08e94841471d00b9e6f94aa241eb3218e
ABC <abc@MBP-cua-ABC.lan>
2024-10-04 16:12:45 +0700

    Setup turn server

--- src/config.ts
+++ src/config.ts
@@ -72,7 +72,7 @@ if (turn) {
 // See https://github.com/feross/simple-peer#peer--new-peeropts
 export const ICE_CONFIG = {
   iceTransportPolicy: 'all',
-  reconnectTimer: 3000,
+  reconnectTimer: 10_000,
 
   // These settings are no secret, since they are readable from the client side anyway
   iceServers,
